f51051f5978d61b31a09978ea017eb2635e56e2c
Gustavo Dantas Galote <gustav0d@protonmail.com>
2023-11-21 23:48:08 -0300

    fix: remove WEB_URL

--- app/config.ts
+++ app/config.ts
@@ -1,18 +1,7 @@
-import { config } from 'dotenv';
-
-config();
-
-const { NODE_ENV, WEB_URL } = process.env;
+const { NODE_ENV } = process.env;
 
 const isDevelopment = NODE_ENV === 'development';
 
-let webUrl = 'http://localhost:3000';
-
-if (!isDevelopment) {
-  if (!WEB_URL) {
-    throw new Error('WEB_URL is not defined');
-  }
-  webUrl = WEB_URL.endsWith('/') ? WEB_URL.slice(0, -1) : WEB_URL;
-}
+const webUrl = isDevelopment ? 'http://localhost:3000' : 'https://dantas15.com';
 
 export { isDevelopment, webUrl };
