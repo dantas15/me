396583b0576512354a7ef21d0f0785e84490099b
Gustavo Dantas Galote <gustav0d@protonmail.com>
2023-12-01 03:25:31 -0300

    feat(portfolio): add basic portfolio data

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,51 @@
+import Link from 'next/link';
+import { ArrowIcon } from './components/ArrowIcon';
+
 export default function Page() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        my portfolio
+        hi, I'm gustavo dantas 👋
       </h1>
       <p className="prose prose-neutral dark:prose-invert">
-        this is the attempt to code my new portfolio :/
+        I'm a future software engineer based in Brazil.
       </p>
+      <br />
+      <p className="prose prose-neutral dark:prose-invert">
+        <Link href="https://ufla.br/" target="_blank">
+          UFLA
+        </Link>{' '}
+        B.Sc. Computer Science. currently <Link href="/work">working</Link> as a
+        Project Director at{' '}
+        <Link href="https://www.linkedin.com/company/compjunior/">
+          Comp Júnior
+        </Link>
+        .
+      </p>
+      <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://bento.me/dantas"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">all my links</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/dantas15/me"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">⭐ this project on github!</p>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,9 +2,48 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
-      <p className="prose prose-neutral dark:prose-invert">
-        Where's my work experience? 🤓
-      </p>
+
+      <div className="mb-8">
+        <h2 className="text-xl font-semibold mb-2">Comp Júnior</h2>
+        <p className="text-gray-600">9 months | Lavras, Minas Gerais, Brazil</p>
+        <p className="prose prose-neutral dark:prose-invert">
+          <strong>Projects Director</strong> | Aug 2023 - Present | 5 months
+          <br />
+          I am primarily responsible for the strategic planning of the projects
+          sector, identifying the feasibility of new projects, and introducing
+          new technologies into Comp Júnior through training and documentation.
+          I also structured a platform for technical documentation and internal
+          management of the Projects Department. Additionally, I organize squads
+          for project development and ensure the quality of deliveries, mainly
+          through Code Review.
+          <br />
+        </p>
+      </div>
+
+      <div>
+        <h2 className="text-xl font-semibold mb-2">IT Technician</h2>
+        <p className="text-gray-600">
+          Jan 2022 - Dec 2022 | 1 year | Amparo, São Paulo, Brazil
+        </p>
+        <p className="prose prose-neutral dark:prose-invert mb-1">
+          This was my first practical experience after completing the technical
+          course, applying theoretical concepts such as design patterns,
+          database modeling, state management with React Context, and web
+          development in general.
+        </p>
+        <p className="prose prose-neutral dark:prose-invert mb-1">
+          I was responsible for structuring the migration of a legacy system in
+          Delphi to the web.
+        </p>
+        <p className="prose prose-neutral dark:prose-invert mb-1">
+          Played a crucial role in selecting technologies for the web system,
+          especially on the front-end.
+        </p>
+        <p className="prose prose-neutral dark:prose-invert">
+          Worked with Typescript, React.js, MantineUI, React Context, Zod,
+          TanStack Query, etc.
+        </p>
+      </div>
     </section>
   );
 }
