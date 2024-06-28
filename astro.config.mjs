ab2ab93212659b61e6a0236fcb8a1176ade8104e
Gustavo Dantas <gusdantas15@gmail.com>
2024-06-28 15:06:58 -0300

    fix(config): update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://dantas15.com',
     integrations: [
         mdx(),
         sitemap(),
