a0101663ad328959552926d188e5e7be86e63b91
Gustavo Dantas <gusdantas15@gmail.com>
2024-07-03 17:07:00 -0300

    feat(cv): generate pdf remotely

--- src/pages/cv.pdf.ts
+++ src/pages/cv.pdf.ts
@@ -1,35 +1,33 @@
 import { readFileSync } from 'node:fs';
 
 import handlebars from 'handlebars';
-import chromium from '@sparticuz/chromium';
 import { DateTime } from 'luxon';
-
-import type { APIRoute } from 'astro';
-import type { Browser } from 'puppeteer';
-import type { Browser as CoreBrowser } from 'puppeteer-core';
 
 import { personalData } from '../data/personal';
 
+import type { APIRoute } from 'astro';
+
+type PDFResponse = {
+    pdf: string;
+};
+
+async function fetchPdf(html: string): Promise<Buffer> {
+    const response = await fetch(import.meta.env.PDF_ENDPOINT, {
+        headers: { 'Content-Type': 'application/json', authorization: import.meta.env.PDF_GEN_API_KEY },
+        method: 'POST',
+        body: JSON.stringify({ html })
+    });
+
+    if (!response.ok) {
+        throw new Error(`PDF Api not working: ${(await response.json()).error}`);
+    }
+    const data: PDFResponse = await response.json();
+
+    return Buffer.from(data.pdf, 'utf-8');
+}
+
 export const GET: APIRoute = async () => {
     const templateHtml = readFileSync('src/hbs/cv-template.hbs', 'utf8');
-
-    let browser: Browser | CoreBrowser;
-    if (process.env.NODE_ENV === 'production') {
-        const puppeteer = await import('puppeteer-core');
-        browser = await puppeteer.launch({
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath(),
-            headless: chromium.headless
-        });
-    } else {
-        const puppeteer = await import('puppeteer');
-        browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-    }
-
-    const page = await browser.newPage();
 
     handlebars.registerHelper('formatDate', function (dateStr) {
         return DateTime.fromISO(dateStr).toFormat('MMM yyyy');
@@ -38,9 +36,7 @@ export const GET: APIRoute = async () => {
     const template = handlebars.compile(templateHtml);
     const html = template(personalData);
 
-    await page.setContent(html);
-    const pdf = await page.pdf({ format: 'A4' });
-    await browser.close();
+    const pdf = await fetchPdf(html);
 
     return new Response(pdf, { headers: { 'Content-Type': 'application/pdf' } });
 };
